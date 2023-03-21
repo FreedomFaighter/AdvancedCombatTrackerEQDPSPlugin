35fbb9526d8396eb482660b988e6a82a65848176
John H. Freeman <johnherschelfreeman2013@gmail.com>
2023-03-21 00:34:28 -0400

    Parses correctly after reset

---
+++ EverQuestSwingType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EverQuestDPSPlugin
+{
+    internal enum EverQuestSwingType : int
+    {
+        Melee = 1,
+        NonMelee = 2,
+        InstantHealing = 4,
+        HealOverTime = 8,
+        Bane = 16,
+        Pet = 32,
+        Warder = 64,
+        Incoming = 128,
+        Outgoing = 256,
+        PetMeleeIncoming = Pet | Melee | Incoming,
+        PetNonMeleeIncoming = Pet | NonMelee,
+        PetMeleeOutgoing = Pet | Melee |Outgoing,
+        PetNonMeleeOutgoing = Pet | NonMelee | Outgoing
+    }
+}
