bc9155b72b677576da647959cb42ca1adc12ba78
John H. Freeman <johnherschelfreeman2013@gmail.com>
2023-05-11 10:35:47 -0700

    Seperated SpecialAttacks and SpellDamageSave enum objects into their own files

---
+++ SpecialAttacks.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACT_EverQuest_DPS_Plugin
+{
+    internal enum SpecialAttacks
+    {
+        Crippling_Blow = 1,
+        Critical = 2,
+        Flurry = 4,
+        Locked = 8,
+        Lucky = 16,
+        Riposte = 32,
+        Strikethrough = 64,
+        Double_Bow_Shot = 128,
+        Twincast = 256
+    }
+}
