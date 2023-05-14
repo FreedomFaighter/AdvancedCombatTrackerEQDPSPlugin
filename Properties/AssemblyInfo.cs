26ab72df0d76bc58d90d05331ba5ca42da7ebd3b
John H. Freeman <johnherschelfreeman2013@gmail.com>
2023-05-14 04:02:38 -0400

    Updated release method with version information change

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.0.5")]
-[assembly: AssemblyFileVersion("0.0.0.6")]
+[assembly: AssemblyVersion("0.0.0.8")]
+[assembly: AssemblyFileVersion("0.0.0.9")]
