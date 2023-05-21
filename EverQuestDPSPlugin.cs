b88bc06ace57d2acb6ef7e32c8370a6fc2f62ecb
FreedomFaighter <johnherschelfreeman2013@gmail.com>
2023-05-21 01:24:24 -0400

    Update EverQuestDPSPlugin.cs

--- EverQuestDPSPlugin.cs
+++ EverQuestDPSPlugin.cs
@@ -500,7 +500,7 @@ namespace ACT_EverQuest_DPS_Plugin
                 {
                     try
                     {
-                        while (await xReader.ReadAsync())
+                        while ((await xReader.ReadAsync()).Result)
                         {
                             if (xReader.NodeType == XmlNodeType.Element)
                             {
