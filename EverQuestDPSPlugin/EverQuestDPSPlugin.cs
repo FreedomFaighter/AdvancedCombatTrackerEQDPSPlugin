8e192afada1beef68cfc1d91f02382335aff9043
John H. Freeman <johnherschelfreeman2013@gmail.com>
2023-05-24 19:32:06 -0400

    Updated order of event assignments to add time stamp parser so it will be available before the log parse event is assigned to the event object

--- EverQuestDPSPlugin/EverQuestDPSPlugin.cs
+++ EverQuestDPSPlugin/EverQuestDPSPlugin.cs
@@ -17,11 +17,12 @@ using System.IO;
 using System.Reflection;
 using System.Xml;
 using System.Collections.Concurrent;
+using System.Collections;
 /*
- * Project: EverQuest DPS Plugin
- * Original: EverQuest 2 English DPS Localization plugin developed by EQAditu
- * Description: Missing from the arsenal of the plugin based Advanced Combat Tracker to track EverQuest's current combat messages.  Ignores chat as that is displayed in game.
- */
+* Project: EverQuest DPS Plugin
+* Original: EverQuest 2 English DPS Localization plugin developed by EQAditu
+* Description: Missing from the arsenal of the plugin based Advanced Combat Tracker to track EverQuest's current combat messages.  Ignores chat as that is displayed in game.
+*/
 
 [assembly: AssemblyTitle("Sample Plugin")]
 [assembly: AssemblyDescription("A sample of an ACT plugin that is a UserControl and uses a settings file")]
@@ -196,9 +197,10 @@ namespace ACT_EverQuest_DPS_Plugin
 
             PopulateRegexArray();
             SetupEverQuestEnvironment();
+            ActGlobals.oFormActMain.GetDateTimeFromLog += new FormActMain.DateTimeLogParser(ParseDateTime);
             ActGlobals.oFormActMain.BeforeLogLineRead += new LogLineEventDelegate(oFormActMain_BeforeLogLineRead);
             ActGlobals.oFormActMain.UpdateCheckClicked += new FormActMain.NullDelegate(UpdateCheckClicked);
-            ActGlobals.oFormActMain.GetDateTimeFromLog += new FormActMain.DateTimeLogParser(ParseDateTime);
+            
             Task updateCheckClicked = new Task(() =>
             {
                 UpdateCheckClicked();
@@ -222,9 +224,10 @@ namespace ACT_EverQuest_DPS_Plugin
 
         public void DeInitPlugin()
         {
+            ActGlobals.oFormActMain.GetDateTimeFromLog -= ParseDateTime;
             ActGlobals.oFormActMain.BeforeLogLineRead -= oFormActMain_BeforeLogLineRead;
             ActGlobals.oFormActMain.UpdateCheckClicked -= UpdateCheckClicked;
-            ActGlobals.oFormActMain.GetDateTimeFromLog -= ParseDateTime;
+            
 
             if (optionsNode != null)    // If we added our user control to the Options tab, remove it
             {
@@ -313,6 +316,10 @@ namespace ACT_EverQuest_DPS_Plugin
 
         void oFormActMain_BeforeLogLineRead(bool isImport, LogLineEventArgs logInfo)
         {
+            //regexTupleList.FirstOrDefault((tuple) =>
+            //{
+            //    return (tuple.Item2.Match(logInfo.logLine)).Success;
+            //});
             for (int i = 0; i < regexTupleList.Count; i++)
             {
                 Match regexMatch = regexTupleList[i].Item2.Match(logInfo.logLine);
