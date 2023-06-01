45e8104d5a48e49d47964e22ec2f3c3a617ac2cd
John H. Freeman <johnherschelfreeman2013@gmail.com>
2023-06-01 08:29:26 -0400

    updated revision number

--- EverQuestDPSPlugin/Properties/AssemblyInfo.cs
+++ EverQuestDPSPlugin/Properties/AssemblyInfo.cs
@@ -27,4 +27,4 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 //[assembly: AssemblyVersion("0.0.0.24")]
-[assembly: AssemblyFileVersion("0.0.0.41")]
+[assembly: AssemblyFileVersion("0.0.0.42")]
