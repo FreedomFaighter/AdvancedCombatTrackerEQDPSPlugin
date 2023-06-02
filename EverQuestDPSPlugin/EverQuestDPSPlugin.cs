ba63daf2e4094fd41fa724fdcd9bcc9aceaab561
FreedomFaighter <johnherschelfreeman2013@gmail.com>
2023-06-01 21:18:06 -0400

    Update EverQuestDPSPlugin.cs

--- EverQuestDPSPlugin/EverQuestDPSPlugin.cs
+++ EverQuestDPSPlugin/EverQuestDPSPlugin.cs
@@ -212,7 +212,7 @@ namespace EverQuestDPSPlugin
 
         void changeLblStatus(String status)
         {
-            if(varianceChkBx.InvokeRequired)
+            if(lblStatus.InvokeRequired)
             {
                 this.lblStatus.Invoke(new Action(() => {
                     this.lblStatus.Text = status;
