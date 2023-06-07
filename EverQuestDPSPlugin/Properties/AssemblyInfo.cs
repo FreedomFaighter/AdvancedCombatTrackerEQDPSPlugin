8d5aa12d973f61d538894e056f39069d3f6f785a
John Herschel Freeman <johnherschelfreeman2013@gmail.com>
2023-06-07 03:50:42 -0400

    updated version attribute

--- EverQuestDPSPlugin/Properties/AssemblyInfo.cs
+++ EverQuestDPSPlugin/Properties/AssemblyInfo.cs
@@ -29,4 +29,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("0.0.0.25")]
 [assembly: AssemblyFileVersion("0.0.0.25")]
 //[assembly: AssemblyVersion("0.0.0.24")]
-[assembly: AssemblyFileVersion("0.0.1.05")]
+[assembly: AssemblyFileVersion("0.0.1.06")]
