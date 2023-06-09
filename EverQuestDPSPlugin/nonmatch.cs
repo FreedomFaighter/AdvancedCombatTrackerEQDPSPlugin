ac9b6899216dbc043b602a4f90ec89108844d421
John Herschel Freeman <johnherschelfreeman2013@gmail.com>
2023-06-09 03:42:39 -0400

    Added window for non match loglines that is only enabled upon a debug build with the DEBUG preprocessor defined

---
+++ EverQuestDPSPlugin/nonmatch.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Advanced_Combat_Tracker;
+
+namespace EverQuestDPSPlugin
+{
+    public partial class nonmatch : Form
+    {
+        public nonmatch()
+        {
+            InitializeComponent();
+        }
+
+        public void addLogLineToForm(String logline)
+        {
+            this.nonMatchList.Items.Add(logline);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
