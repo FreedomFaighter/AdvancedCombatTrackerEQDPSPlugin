0fe8ad5810dc0a9cca4c6ef9537049ed1b5c3f54
John Herschel Freeman <johnherschelfreeman2013@gmail.com>
2023-06-13 01:20:26 -0400

    reverted to 0.0.1.09

---
+++ EverQuestDPSPlugin/Enums/SpellDamageSave.cs
@@ -0,0 +1,16 @@
+﻿//Spell damage saves currently known
+//Chromatic is a combination of these enum values and may be Fire | Cold = 3
+
+namespace EverQuestDPSPlugin.Enums
+{
+    internal enum SpellDamageSave
+    {
+        Fire = 1,
+        Cold = 2,
+        Poison = 4,
+        Disease = 8,
+        Magic = 16,
+        Corruption = 32,
+        Unresistable = 64
+    }
+}
