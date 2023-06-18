04001d26a88530da3d34dc5f92ae6f959307deb3
John H. Freeman <john@freemania.pro>
2023-06-18 02:46:25 -0400

    Added two new tests to EnumUnitTest

--- EQDPSPluginUnitTests/PluginEnumUnitTests.cs
+++ EQDPSPluginUnitTests/PluginEnumUnitTests.cs
@@ -10,7 +10,19 @@ namespace EQDPSPluginUnitTests
         [TestMethod]
         public void TestMeleeAndPetEnumCharacterPossesiveTypeAttackMethod()
         {
-            Assert.AreEqual(EverQuestSwingType.Pet.CharacterPossesiveTypeAttack(EverQuestSwingType.Melee), (EverQuestSwingType.Pet | EverQuestSwingType.Melee));
+            Assert.AreEqual(EverQuestSwingType.Pet.CharacterPossesiveTypeAttack(EverQuestSwingType.Melee), EverQuestSwingType.PetMelee);
+        }
+
+        [TestMethod]
+        public void TestInstantHealingAndWardEnumCharacterPossessiveTypeAttackMethod()
+        {
+            Assert.AreEqual(EverQuestSwingType.Ward.CharacterPossesiveTypeAttack(EverQuestSwingType.InstantHealing), EverQuestSwingType.WardInstantHealing);
+        }
+
+        [TestMethod]
+        public void TestHealingOverTimeAndWardEnumCharacterPossessiveTypeAttackMethod()
+        {
+            Assert.AreEqual(EverQuestSwingType.Ward.CharacterPossesiveTypeAttack(EverQuestSwingType.HealOverTime), EverQuestSwingType.WardHealOverTime);
         }
 
         private EverQuestDPSPlugin.EverQuestDPSPlugin _everQuestDPSPlugin;
