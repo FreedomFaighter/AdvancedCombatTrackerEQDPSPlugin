2099f2e19846efe9d8d4767fdae96f9fcc25b725
FreedomFaighter <johnherschelfreeman2013@gmail.com>
2023-10-28 16:03:23 -0400

    updated version number for application update method

--- EverQuestDPSPlugin/Properties/AssemblyInfo.cs
+++ EverQuestDPSPlugin/Properties/AssemblyInfo.cs
@@ -25,5 +25,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.1.0.30")]
+[assembly: AssemblyVersion("0.1.0.32")]
 [assembly: InternalsVisibleTo("EQDPSPluginUnitTests")]
