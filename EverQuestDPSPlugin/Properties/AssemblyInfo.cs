e29b956f3908430554b078c49cb1ed4f00e5e56a
FreedomFaighter <johnherschelfreeman2013@gmail.com>
2023-11-22 01:52:50 -0500

    Updated version info

--- EverQuestDPSPlugin/Properties/AssemblyInfo.cs
+++ EverQuestDPSPlugin/Properties/AssemblyInfo.cs
@@ -25,5 +25,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.2.3.*")]
+[assembly: AssemblyVersion("0.2.4.*")]
 [assembly: InternalsVisibleTo("EQDPSPluginUnitTests")]
