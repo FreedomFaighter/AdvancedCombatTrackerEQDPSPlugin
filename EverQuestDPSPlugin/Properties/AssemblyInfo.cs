1b7c5dbad76877c4ae1d75f7b19bb5c8111aafc3
John H. Freeman <john@freemania.pro>
2024-01-20 01:07:47 -0500

    updated version number

--- EverQuestDPSPlugin/Properties/AssemblyInfo.cs
+++ EverQuestDPSPlugin/Properties/AssemblyInfo.cs
@@ -25,5 +25,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.9.*")]
+[assembly: AssemblyVersion("1.0.10.*")]
 [assembly: InternalsVisibleTo("EQDPSPluginUnitTests")]
