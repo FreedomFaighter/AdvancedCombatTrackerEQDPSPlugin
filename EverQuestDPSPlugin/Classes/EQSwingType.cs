d92e625513a455a8cbfe744bcd2faf33f3cc4717
John H. Freeman <john@freemania.pro>
2024-10-08 16:34:36 -0400

    converted enum with bitmasks to class with static values

--- EverQuestDPSPlugin/Classes/EQSwingType.cs
+++ EverQuestDPSPlugin/Classes/EQSwingType.cs
@@ -1,29 +1,30 @@
 ﻿using System;
 
-namespace EverQuestDPS.Enums
+namespace EverQuestDPS.Classes
 {
-    [Flags]
-    public enum EQSwingType : int
+    public class EQSwingType
     {
-        Melee = 1,
-        NonMelee = 2,
-        Healing = 4,
-        Spell = 8,
-        Bane = 16,
-        DamageShield = 32,
-        Instant = 64,
-        OverTime = 128,
-        HealingOverTime = Healing | OverTime,
-        InstantHealing = Healing | Instant,
-        DirectDamageSpell = Spell | Instant,
-        SpellOverTime = Spell | OverTime,
-    }
+        static readonly int melee = 1;
+        static readonly int nonMelee = 2;
+        static readonly int healing = 4;
+        static readonly int spell = 8;
+        static readonly int bane = 16;
+        static readonly int damageShield = 32;
+        static readonly int instant = 64;
+        static readonly int overTime = 128;
+        static readonly int healingOverTime = healing + overTime;
+        static readonly int instantHealing = healing + instant;
+        static readonly int directDamageSpell = spell + instant;
+        static readonly int spellOverTime = spell + overTime;
 
-    internal static class EQSwingTypeExtensions
-    {
-        internal static int GetEQSwingTypeExtensionIntValue(this EQSwingType type)
-        {
-            return (int)type;
-        }
+        public int Melee {  get { return melee; } }
+        public int NonMelee { get {return nonMelee; } }
+        public int Healing { get { return healing; } }
+        public int Spell { get { return spell; } }
+        public int DamageShield { get { return damageShield; } }
+        public int InstantHealing { get {return instantHealing; } }
+        public int HealingOverTime { get { return healingOverTime; } }
+        public int DirectDamageSpell { get { return directDamageSpell; } }
+        public int SpellOverTime { get {return spellOverTime; } }
     }
 }
